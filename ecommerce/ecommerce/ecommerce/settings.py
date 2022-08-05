346c3faaf3b343a7912f2f0e89d6678162884822
kl373 <kl373@duke.edu>
2022-08-04 21:52:24 -0400

    add database connection

--- ecommerce/ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/ecommerce/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,14 +76,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
+GRAPHENE = {
+    "SCHEMA": "products.schema.schema",
+    }
+    
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'env',
+        'USER': 'postgres',
+        'PASSWORD': 'kunyu315',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        "ATOMIC_MUTATIONS": True
     }
 }
 
@@ -125,3 +141,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- ecommerce/ecommerce/products/models.py
+++ ecommerce/ecommerce/products/models.py
@@ -9,48 +9,8 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
     def __str__(self):
         return self.title
-"""
-class Book(models.Model):
-    title = models.CharField(max_length=150)
-    author = models.CharField(max_length=100, default='John Doe')
-    isbn = models.CharField(max_length=13)
-    pages = models.IntegerField()
-    price = models.IntegerField()
-    quantity = models.IntegerField()
-    description = models.TextField()
-    status = models.BooleanField()
-    date_created = models.DateField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['-date_created']
 
-    def __str__(self):
-        return self.title
-
-class Grocery(models.Model):
-    product_tag = models.CharField(max_length=10)
-    name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, related_name='grocery', on_delete=models.CASCADE)
-    price = models.IntegerField()
-    quantity = models.IntegerField()
-    imageurl = models.URLField()
-    status = models.BooleanField()
-    date_created = models.DateField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-date_created']
-
-    def __str__(self):
-        return self.name
-   
-class StockName(models.Model):
-    name = models.CharField(max_length = 255)
-    class Meta:
-        verbose_name_plural = 'StocksName'
-
-    def __str__(self):
-        return self.name
-"""
 class TradeInfo(models.Model):
     Name = models.CharField(max_length=255)
     Date = models.DateField(auto_now_add=False)
